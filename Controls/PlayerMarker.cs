2cb87bf9b6b180b9a954f69ad207756c2ce1a0c0
Robert Macdonald <robbie94macdonald@gmail.com>
2023-02-20 22:12:23 +1300

    Butchered 'distance rings' module into a 'player marker'

---
+++ Controls/PlayerMarker.cs
@@ -0,0 +1,101 @@
+﻿using Blish_HUD;
+using Blish_HUD.Entities;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+
+namespace Tortle.PlayerMarker.Control
+{
+    public class PlayerMarker : IEntity
+    {
+        public Texture2D MarkerTexture;
+        public float MarkerOpacity;
+        public bool Visible;
+        public Color MarkerColor;
+        private VertexPositionColorTexture[] vertex;
+
+        private VertexBuffer _geometryBuffer;
+
+        public Vector3 Size { get; set; }
+
+        public float VerticalOffset { get; set; }
+
+        public float DrawOrder => 0;
+
+        private static BasicEffect _renderEffect;
+
+        public PlayerMarker()
+        {
+            _renderEffect = _renderEffect ?? new BasicEffect(GameService.Graphics.GraphicsDevice);
+
+            this.Size = Vector3.Zero;
+            this.MarkerOpacity = 1f;
+            this.MarkerColor = Color.White;
+            this.Visible = false;
+
+            _renderEffect.TextureEnabled = true;
+            _renderEffect.VertexColorEnabled = true;
+
+            vertex = new VertexPositionColorTexture[4];
+        }
+
+        public void UpdateRings()
+        {
+            vertex[0].Position = new Vector3(-1, 1, 1) * Size;
+            vertex[0].TextureCoordinate = new Vector2(0, 0);
+            vertex[0].Color = MarkerColor;
+            vertex[1].Position = new Vector3(1, 1, 1) * Size;
+            vertex[1].TextureCoordinate = new Vector2(1, 0);
+            vertex[1].Color = MarkerColor;
+            vertex[2].Position = new Vector3(-1, -1, 1) * Size;
+            vertex[2].TextureCoordinate = new Vector2(0, 1);
+            vertex[2].Color = MarkerColor;
+            vertex[3].Position = new Vector3(1, -1, 1) * Size;
+            vertex[3].TextureCoordinate = new Vector2(1, 1);
+            vertex[3].Color = MarkerColor;
+
+            _geometryBuffer = new VertexBuffer(GameService.Graphics.GraphicsDevice, VertexPositionColorTexture.VertexDeclaration, 4, BufferUsage.WriteOnly);
+            _geometryBuffer.SetData(vertex);
+        }
+
+
+
+        public void Render(GraphicsDevice graphicsDevice, IWorld world, ICamera camera)
+        {
+            if (this.Visible && _geometryBuffer != null)
+            {
+                float x = GameService.Gw2Mumble.PlayerCharacter.Position.X;
+                float y = GameService.Gw2Mumble.PlayerCharacter.Position.Y;
+                float z = GameService.Gw2Mumble.PlayerCharacter.Position.Z + VerticalOffset;
+
+                float facing = (float)(Math.Atan2(GameService.Gw2Mumble.PlayerCamera.Forward.Y, GameService.Gw2Mumble.PlayerCamera.Forward.X));
+                float facing2 = (float)(Math.Tan(GameService.Gw2Mumble.PlayerCamera.Forward.Z));
+                Matrix worldMatrix = Matrix.CreateTranslation(x, y, z);
+
+                var rotationMatrixX = Matrix.CreateRotationX((float)(facing2 - 2 * Math.PI / 4));
+                var rotationMatrixZ = Matrix.CreateRotationZ((float)(facing - 2 * Math.PI / 4));
+
+                var rotationMatrix = Matrix.Multiply(rotationMatrixX, rotationMatrixZ);
+                worldMatrix = Matrix.Multiply(rotationMatrix, worldMatrix);
+
+                _renderEffect.View = GameService.Gw2Mumble.PlayerCamera.View;
+                _renderEffect.Projection = GameService.Gw2Mumble.PlayerCamera.Projection;
+                _renderEffect.World = worldMatrix;
+                _renderEffect.Texture = MarkerTexture;
+                _renderEffect.Alpha = MarkerOpacity;
+
+                graphicsDevice.SetVertexBuffer(_geometryBuffer, 0);
+
+                foreach (var pass in _renderEffect.CurrentTechnique.Passes)
+                    pass.Apply();
+
+                graphicsDevice.DrawPrimitives(
+                    PrimitiveType.TriangleStrip,
+                    0,
+                    2);
+            }
+        }
+
+        public void Update(GameTime gameTime) { /* NOOP */ }
+    }
+}
---
+++ Views/SettingsView.cs
@@ -0,0 +1,176 @@
+﻿using Blish_HUD.Controls;
+using Microsoft.Xna.Framework;
+using Blish_HUD.Graphics.UI;
+using Blish_HUD.Input;
+using Blish_HUD.Settings;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Tortle.PlayerMarker.Views
+{
+    public class SettingsView : View
+    {
+        Panel colorPickerPanel;
+        ColorPicker colorPicker;
+        private ColorBox settingPlayerMarkerColor_Box;
+        SettingEntry<string> colorBoxSelected = new SettingEntry<string>();
+
+        protected override void Build(Container buildPanel) {
+            var parentPanel = new Panel() {
+                CanScroll = false,
+                Parent = buildPanel,
+                Height = buildPanel.Height,
+                HeightSizingMode = SizingMode.AutoSize,
+                Width = 700,  //bug? with buildPanel.Width changing to 40 after loading a different module settings and coming back.,
+            };
+            parentPanel.LeftMouseButtonPressed += delegate {
+                if (colorPickerPanel.Visible && !colorPickerPanel.MouseOver && !settingPlayerMarkerColor_Box.MouseOver)
+                    colorPickerPanel.Visible = false;
+            };
+
+            colorPickerPanel = new Panel() {
+                Location = new Point(parentPanel.Width - 420 - 10, 10),
+                Size = new Point(420, 255),
+                Visible = false,
+                ZIndex = 10,
+                Parent = parentPanel,
+                BackgroundTexture = PlayerMarkerModule.ModuleInstance.ContentsManager.GetTexture("155976.png"),
+                ShowBorder = false,
+            };
+            var colorPickerBG = new Panel() {
+                Location = new Point(15, 15),
+                Size = new Point(colorPickerPanel.Size.X - 35, colorPickerPanel.Size.Y - 30),
+                Parent = colorPickerPanel,
+                BackgroundTexture = PlayerMarkerModule.ModuleInstance.ContentsManager.GetTexture("buttondark.png"),
+                ShowBorder = true,
+            };
+            colorPicker = new ColorPicker() {
+                Location = new Point(10, 10),
+                CanScroll = false,
+                Size = new Point(colorPickerBG.Size.X - 20, colorPickerBG.Size.Y - 20),
+                Parent = colorPickerBG,
+                ShowTint = false,
+                Visible = true
+            };
+            colorPicker.SelectedColorChanged += delegate {
+                colorPicker.AssociatedColorBox.Color = colorPicker.SelectedColor;
+                try {
+                    colorBoxSelected.Value = colorPicker.SelectedColor.Name;
+                } catch {
+                    colorBoxSelected.Value = "White0";
+                }
+                colorPickerPanel.Visible = false;
+            };
+            colorPicker.LeftMouseButtonPressed += delegate {
+                colorPickerPanel.Visible = false;
+            };
+            foreach (var color in PlayerMarkerModule._colors) {
+                colorPicker.Colors.Add(color);
+            }
+
+            var settingPlayerMarkerEnable_Label = new Label()
+            {
+	            Location = new Point(10, 18),
+	            Width = 100,
+                HorizontalAlignment = HorizontalAlignment.Right,
+	            WrapText = false,
+	            Parent = parentPanel,
+	            Text = "Enable Marker: ",
+            };
+            var settingPlayerMarkerEnable_Checkbox = new Checkbox() {
+                Location = new Point(settingPlayerMarkerEnable_Label.Right + 5, settingPlayerMarkerEnable_Label.Top + 2),
+                Parent = parentPanel,
+                Checked = PlayerMarkerModule._settingPlayerMarkerEnable.Value
+            };
+            settingPlayerMarkerEnable_Checkbox.CheckedChanged += delegate {
+                PlayerMarkerModule._settingPlayerMarkerEnable.Value = settingPlayerMarkerEnable_Checkbox.Checked;
+            };
+
+            var settingPlayerMarkerRadius_Label = new Label() {
+                Location = new Point(settingPlayerMarkerEnable_Label.Left, settingPlayerMarkerEnable_Label.Bottom + 5),
+                Width = 100,
+                AutoSizeHeight = false,
+                WrapText = false,
+                Parent = parentPanel,
+                Text = "Radius: ",
+                HorizontalAlignment = HorizontalAlignment.Right,
+            };
+            var settingsPlayerMarkerRadius_Slider = new TrackBar()
+            {
+	            Location = new Point(settingPlayerMarkerRadius_Label.Right + 5, settingPlayerMarkerRadius_Label.Top + 2),
+	            Width = 250,
+	            MaxValue = 40,
+	            MinValue = 1,
+	            Value = PlayerMarkerModule._settingPlayerMarkerRadius.Value,
+	            Parent = parentPanel,
+            };
+            settingsPlayerMarkerRadius_Slider.ValueChanged += delegate
+            {
+	            PlayerMarkerModule._settingPlayerMarkerRadius.Value = settingsPlayerMarkerRadius_Slider.Value;
+            };
+
+            var settingPlayerMarkerColor_Label = new Label()
+            {
+	            Location = new Point(settingPlayerMarkerEnable_Label.Left, settingPlayerMarkerRadius_Label.Bottom + 5),
+	            Width = 100,
+	            AutoSizeHeight = false,
+	            WrapText = false,
+	            Parent = parentPanel,
+	            Text = "Color: ",
+	            HorizontalAlignment = HorizontalAlignment.Right,
+            };
+            settingPlayerMarkerColor_Box = new ColorBox() {
+                Location = new Point(settingPlayerMarkerColor_Label.Right + 5, settingPlayerMarkerColor_Label.Top - 5),
+                Parent = parentPanel,
+                Color = PlayerMarkerModule._colors.Find(x => x.Name.Equals(PlayerMarkerModule._settingPlayerMarkerColor.Value)),
+            };
+            settingPlayerMarkerColor_Box.Click += delegate (object sender, MouseEventArgs e) {
+                SetColorSetting(ref PlayerMarkerModule._settingPlayerMarkerColor);
+                colorPicker.AssociatedColorBox = (ColorBox)sender;
+                colorPickerPanel.Visible = !colorPickerPanel.Visible;
+            };
+
+            var settingPlayerMarkerOpacity_Label = new Label() {
+                Location = new Point(settingPlayerMarkerEnable_Label.Left, settingPlayerMarkerColor_Box.Bottom + 5),
+                Width = 100,
+                AutoSizeHeight = false,
+                WrapText = false,
+                Parent = parentPanel,
+                Text = "Opacity: ",
+                HorizontalAlignment = HorizontalAlignment.Right,
+            };
+            var settingPlayerMarkerOpacity_Slider = new TrackBar() {
+                Location = new Point(settingPlayerMarkerOpacity_Label.Right + 5, settingPlayerMarkerOpacity_Label.Top + 2),
+                Width = 250,
+                MaxValue = 100,
+                MinValue = 0,
+                Value = PlayerMarkerModule._settingPlayerMarkerOpacity.Value * 100,
+                Parent = parentPanel,
+            };
+            settingPlayerMarkerOpacity_Slider.ValueChanged += delegate { PlayerMarkerModule._settingPlayerMarkerOpacity.Value = settingPlayerMarkerOpacity_Slider.Value / 100; };
+
+            var settingPlayerMarkerVerticalOffset_Label = new Label() {
+                Location = new Point(settingPlayerMarkerEnable_Label.Left, settingPlayerMarkerOpacity_Label.Bottom + 5),
+                Width = 100,
+                AutoSizeHeight = false,
+                WrapText = false,
+                Parent = parentPanel,
+                Text = "Vertical Offset: ",
+                HorizontalAlignment = HorizontalAlignment.Right,
+            };
+            var settingPlayerMarkerVerticalOffset_Slider = new TrackBar() {
+                Location = new Point(settingPlayerMarkerVerticalOffset_Label.Right + 5, settingPlayerMarkerVerticalOffset_Label.Top + 2),
+                Width = 250,
+                MaxValue = 40,
+                MinValue = 0,
+                Value = (PlayerMarkerModule._settingPlayerMarkerVerticalOffset.Value * 5) + 10,
+                Parent = parentPanel,
+            };
+            settingPlayerMarkerVerticalOffset_Slider.ValueChanged += delegate { PlayerMarkerModule._settingPlayerMarkerVerticalOffset.Value = (settingPlayerMarkerVerticalOffset_Slider.Value - 10) / 5; };
+        }
+
+        private void SetColorSetting(ref SettingEntry<string> setting) {
+            colorBoxSelected = setting;
+        }
+
+    }
+}
