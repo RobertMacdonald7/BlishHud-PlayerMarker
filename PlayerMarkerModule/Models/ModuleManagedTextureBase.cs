2d641a3844b3f5fbec6c58b8e976b0653d2442ec
Robert Macdonald <robbie94macdonald@gmail.com>
2025-03-18 19:14:11 +1300

    Cleanup disposable implementations

--- PlayerMarkerModule/Models/ModuleManagedTextureBase.cs
+++ PlayerMarkerModule/Models/ModuleManagedTextureBase.cs
@@ -1,9 +1,10 @@
-﻿using Blish_HUD;
+﻿using System;
+using Blish_HUD;
 using Blish_HUD.Content;
 
 namespace Tortle.PlayerMarker.Models
 {
-	internal abstract class ModuleManagedTextureBase
+	internal abstract class ModuleManagedTextureBase : IDisposable
 	{
 		protected AsyncTexture2D _texture;
 
@@ -12,6 +13,14 @@ namespace Tortle.PlayerMarker.Models
 
 		public void Dispose()
 		{
+			Dispose(true);
+			GC.SuppressFinalize(this);
+		}
+
+		protected virtual void Dispose(bool disposing)
+		{
+			if (!disposing) return;
+
 			// Don't dispose BlishHUD's error texture!
 			if (_texture?.Texture != ContentService.Textures.Error)
 			{
